2105b45c7ef6910ae937dbaa33c4a77f1d61897b
noa-halevi <noahalv123@gmail.com>
2025-01-06 16:22:08 +0200

    Update User.java

--- User.java
+++ User.java
@@ -44,7 +44,7 @@
     /** If this user follows the given name, returns true; otherwise returns false. */
     public boolean follows(String name) {
         for(int i=0; i < fCount; i++){
-            if(this.follows[i].equals(name)){
+            if(this.follows[i].toLowerCase().equals(name.toLowerCase())){
                 return true;
             }
         }
