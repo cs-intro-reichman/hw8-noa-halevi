6ed31f36d4eb2903d970ee7b50bfb856eca88b27
noa-halevi <noahalv123@gmail.com>
2025-01-08 22:28:00 +0200

    Update Network.java

--- Network.java
+++ Network.java
@@ -1,20 +1,149 @@
-String ans = "Network:";
+/** Represents a social network. The network has users, who follow other uesrs.
+ *  Each user is an instance of the User class. */
+public class Network {
+
+    // Fields
+    private User[] users;  // the users in this network (an array of User objects)
+    private int userCount; // actual number of users in this network
+
+    /** Creates a network with a given maximum number of users. */
+    public Network(int maxUserCount) {
+        this.users = new User[maxUserCount];
+        this.userCount = 0;
+    }
+
+    /** Creates a network  with some users. The only purpose of this constructor is 
+     *  to allow testing the toString and getUser methods, before implementing other methods. */
+    public Network(int maxUserCount, boolean gettingStarted) {
+        this(maxUserCount);
+        users[0] = new User("Foo");
+        users[1] = new User("Bar");
+        users[2] = new User("Baz");
+        userCount = 3;
+    }
+
+    public int getUserCount() {
+        return this.userCount;
+    }
+    /** Finds in this network, and returns, the user that has the given name.
+     *  If there is no such user, returns null.
+     *  Notice that the method receives a String, and returns a User object. */
+    public User getUser(String name) {
+        for(int i = 0; i < users.length; i++){
+            if (users[i] != null && users[i].getName().toLowerCase().equals(name.toLowerCase()) ) {
+                return users[i];
+            }
+        }
+        return null;
+    }
+
+    /** Adds a new user with the given name to this network.
+    *  If ths network is full, does nothing and returns false;
+    *  If the given name is already a user in this network, does nothing and returns false;
+    *  Otherwise, creates a new user with the given name, adds the user to this network, and returns true. */
+    public boolean addUser(String name) {
+        if (name == null || name.isEmpty()) {  
+            return false;
+        }
+        for( int i = 0; i < users.length; i++){
+            if(users[i] != null && users[i].getName().toLowerCase().equals(name.toLowerCase())){
+                return false;
+            }
+        }
+        for( int i = 0; i < users.length; i++){
+            if(users[i] == null){
+                users[i] = new User(name);
+                userCount++;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /** Makes the user with name1 follow the user with name2. If successful, returns true.
+     *  If any of the two names is not a user in this network,
+     *  or if the "follows" addition failed for some reason, returns false. */
+    public boolean addFollowee(String name1, String name2) {
+        if (name1 == null || name2 == null || name1.equals(name2)) { 
+            return false;
+        }
+        User user1 = getUser(name1); 
+        User user2 = getUser(name2); 
+        
+        if (user1 == null || user2 == null) {
+            return false;
+        }
+        return user1.addFollowee(name2);
+    }
+    
+    /** For the user with the given name, recommends another user to follow. The recommended user is
+     *  the user that has the maximal mutual number of followees as the user with the given name. */
+    public String recommendWhoToFollow(String name) {
+        User user1 = getUser(name);
+        if(user1 == null){
+            return null;
+        }
+        String mostRecommendedUserToFollow = "";
+        int maxMutal = 0;
+        for(int i=0; i<userCount; i++){
+            if(users[i] != null && users[i] == user1){
+                continue;
+            }
+            int currentMutal = user1.countMutual(users[i]);
+            if(currentMutal > maxMutal){
+                maxMutal = currentMutal;
+                mostRecommendedUserToFollow = users[i].getName();
+            }
+        }
+        return mostRecommendedUserToFollow;
+    }
+
+    /** Computes and returns the name of the most popular user in this network: 
+     *  The user who appears the most in the follow lists of all the users. */
+    public String mostPopularUser() {
+        String mostPopular = null;
+        int countPopular = 0;
+        for(int i=0; i < userCount; i++){
+            String currentUser = users[i].getName();
+            int check = followeeCount(currentUser);
+            if(check > countPopular){
+                countPopular = check;
+                mostPopular = currentUser;
+            }
+        }
+        return (mostPopular != null) ? mostPopular : "null";
+    }
+
+    /** Returns the number of times that the given name appears in the follows lists of all
+     *  the users in this network. Note: A name can appear 0 or 1 times in each list. */
+    private int followeeCount(String name) {
+        int count = 0;
+        for(int i=0; i < userCount; i++){
+            if(users[i].follows(name)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // Returns a textual description of all the users in this network, and who they follow.
+    public String toString() {
+    StringBuilder ans = new StringBuilder("Network:");
     for (int i = 0; i < users.length; i++) {
         if (users[i] != null) {
-            if (i > 0) {
-                ans += "\n"; // הוספת קו רווח רק לפני משתמשים נוספים
-            }
+            ans.append("\n"); 
             String userName = users[i].getName();
-            ans += userName + " -> ";
+            ans.append(userName).append(" -> ");
             if (users[i].getfCount() > 0) {
                 for (int j = 0; j < users[i].getfCount(); j++) {
-                    ans += users[i].getfFollows()[j];
+                    ans.append(users[i].getfFollows()[j]);
                     if (j < users[i].getfCount() - 1) {
-                        ans += " "; // הוספת רווח רק אם יש עוד עוקבים
+                        ans.append(" "); 
                     }
                 }
             }
         }
     }
-    return ans;
+    return ans.toString();
+    }  
 }
